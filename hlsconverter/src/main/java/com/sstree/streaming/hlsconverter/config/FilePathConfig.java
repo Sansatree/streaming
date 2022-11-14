289261060b9d9e71dbc6d58f2e7fe688cd3bfc4a
Sansatree <70942899+Sansatree@users.noreply.github.com>
2022-11-15 01:05:17 +0900

    Update FilePathConfig.java

--- hlsconverter/src/main/java/com/sstree/streaming/hlsconverter/config/FilePathConfig.java
+++ hlsconverter/src/main/java/com/sstree/streaming/hlsconverter/config/FilePathConfig.java
@@ -14,6 +14,8 @@ public class FilePathConfig {
     public String getFfmpeg() {
         return ffmpeg;
     }
+    
+    //ffmpeg 주소
     @Value("${path.ffmpeg}")
     public void setFfmpeg(String ffmpeg) {
         this.ffmpeg = ffmpeg;
@@ -22,6 +24,8 @@ public class FilePathConfig {
     public String getVideo() {
         return video;
     }
+    
+    // video 저장 위치
     @Value("${path.video}")
     public void setVideo(String video) {
         this.video = video;
@@ -30,6 +34,8 @@ public class FilePathConfig {
     public String getOutput() {
         return output;
     }
+    
+    // .ts 파일 저장될 위치
     @Value("${path.output}")
     public void setOutput(String output) {
         this.output = output;
